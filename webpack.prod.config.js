5473030ac393d1afea0bc627e65ff736d858f3fc
Dominik Zen <zen.dominik@gmail.com>
2020-04-24 17:33:27 +0200

    Update packages, update webpack config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,8 +1,11 @@
 var config = require('./webpack.config.js')
+var MiniCssExtractPlugin = require('mini-css-extract-plugin')
 var WebpackBundleTracker = require('webpack-bundle-tracker')
 
 config.mode = 'production'
+config.output.publicPath = ''
 config.plugins = [
+  new MiniCssExtractPlugin({filename: '[name]_[hash].css'}),
   new WebpackBundleTracker({filename: './webpack-stats-prod.json'}),
 ]
 module.exports = config
