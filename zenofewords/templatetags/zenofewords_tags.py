c18e1af457a12766880a19945e2ff2786d8efc23
Dominik Zen <zen.dominik@gmail.com>
2020-08-05 11:53:21 +0200

    Add work page content, add template tags

--- zenofewords/templatetags/zenofewords_tags.py
+++ zenofewords/templatetags/zenofewords_tags.py
@@ -4,7 +4,7 @@ register = template.Library()
 
 
 @register.inclusion_tag('zenofewords/tags/external_link_tag.html')
-def external_link_tag(url, title, terminator):
+def external_link_tag(url, title, terminator=''):
     return {
         'url': url,
         'title': title,
@@ -18,3 +18,17 @@ def mobile_menu_link_tag(name, path):
         'name': name,
         'current': name in path or name == 'home' and '/' == path,
     }
+
+
+@register.inclusion_tag('zenofewords/tags/jump_to_link_tag.html')
+def jump_to_link_tag(css_class, aria_label, url='#top'):
+    return {
+        'css_class': css_class,
+        'aria_label': aria_label,
+        'url': url,
+    }
+
+
+@register.inclusion_tag('zenofewords/tags/content_block_tag.html')
+def content_block_tag():
+    return {}
